5ec738e71fa9f27e38e0b915937bf61be7a245a3
Nile Daley <niledaley@outlook.com>
2017-09-29 15:29:00 +0100

    Initial project configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
-
+import { DataService } from './data.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  users: Array<any>;
   title = 'app';
+
+  constructor(private _dataService: DataService) {
+    this._dataService.getUsers()
+      .subscribe(res => this.users = res);
+  }
 }
