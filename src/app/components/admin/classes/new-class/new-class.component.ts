9d3ad53d4ccfdd70839f9eaaa1a0fe064ffac160
Nile Daley <NileDaley@outlook.com>
2018-02-14 20:02:21 +0000

    Added some fields to new class component

--- src/app/components/admin/classes/new-class/new-class.component.ts
+++ src/app/components/admin/classes/new-class/new-class.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from 'app/services/data.service';
+import {Tutor} from 'app/models/tutor';
+import {Location} from 'app/models/location';
+import {ClassType} from 'app/models/class-type';
+import {User} from '../../../../models/user';
+import {Skill} from '../../../../models/skill';
+import {OpenHours} from '../../../../models/openHours';
+import {Venue} from '../../../../models/venue';
+import {Class} from '../../../../models/class';
 
 @Component({
   selector: 'app-new-class',
@@ -7,9 +16,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewClassComponent implements OnInit {
 
-  constructor() { }
+  loading: boolean;
 
-  ngOnInit() {
+  tutors: Array<Tutor> = null;
+  locations: Array<Location> = null;
+  classTypes: Array<ClassType> = null;
+  _class: Class = new Class(
+    null,
+    null,
+    null,
+    [],
+    new Date(),
+    new Date(),
+    0,
+    0.00,
+    null,
+    null
+  );
+
+  /*
+    TODO: Add locations and show relevant venues
+    TODO: Set max class limit to size of currently selected venue
+    TODO: Set min date for start date
+   */
+
+  constructor(private _dataService: DataService) {
   }
 
+  ngOnInit() {
+    this.loading = true;
+    this.getTutors();
+    this.getLocations();
+    this.getClassTypes();
+  }
+
+  private getTutors() {
+    this._dataService.getTutors()
+      .subscribe(res => {
+        const data = res['data'];
+        this.tutors = data.map(t => {
+          return new Tutor(
+            t._id,
+            t.forename,
+            t.surname,
+            t.gender,
+            t.phone,
+            new User(t.user.email, null, t.user.role),
+            t.skills.map(s => new Skill(s._id, s.name, s.description))
+          );
+        });
+        this.checkLoadingStatus();
+      });
+  }
+
+  private getLocations() {
+    this._dataService.getLocations()
+      .subscribe(res => {
+        const data = res['data'];
+        this.locations = data.map(l => {
+          return new Location(
+            l._id,
+            l.name,
+            l.address,
+            l.email,
+            l.phone,
+            l.openHours.map(o => {
+              return new OpenHours(o.day, o.isOpen, o.open, o.close);
+            }),
+            l.venues.map(v => {
+              return new Venue(v.name, v.capacity);
+            })
+          );
+        });
+        this.checkLoadingStatus();
+      });
+  }
+
+  private getClassTypes() {
+    this._dataService.getClassTypes()
+      .subscribe(res => {
+        const data = res['data'];
+        this.classTypes = data.map(ct => {
+          return new ClassType(ct._id, ct.name, ct.description);
+        });
+        this.checkLoadingStatus();
+      });
+  }
+
+  private checkLoadingStatus() {
+    this.loading = this.tutors === null
+      || this.locations === null
+      || this.classTypes === null;
+  }
+
+  selectValue(field, val) {
+    this._class[field] = val;
+    console.log(this._class);
+  }
 }
