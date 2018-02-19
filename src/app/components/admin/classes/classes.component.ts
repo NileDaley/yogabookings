c24d1b7b271f400a9691c1333be52e70b60046f6
Nile Daley <NileDaley@outlook.com>
2018-02-19 20:46:37 +0000

    Changed format of full calendar css import

--- src/app/components/admin/classes/classes.component.ts
+++ src/app/components/admin/classes/classes.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { DataService } from 'app/services/data.service';
-import { Class } from 'app/models/class';
-import { ClassType } from 'app/models/class-type';
-import { Tutor } from 'app/models/tutor';
-import { User } from 'app/models/user';
-import { Customer } from 'app/models/customer';
-import { Skill } from 'app/models/skill';
-import { Location } from 'app/models/location';
-import { OpenHours } from 'app/models/openHours';
-import { Venue } from 'app/models/venue';
-import { CalendarComponent } from 'ng-fullcalendar';
-import { Options } from 'fullcalendar';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {DataService} from 'app/services/data.service';
+import {Class} from 'app/models/class';
+import {ClassType} from 'app/models/class-type';
+import {Tutor} from 'app/models/tutor';
+import {User} from 'app/models/user';
+import {Customer} from 'app/models/customer';
+import {Skill} from 'app/models/skill';
+import {Location} from 'app/models/location';
+import {OpenHours} from 'app/models/openHours';
+import {Venue} from 'app/models/venue';
+import {CalendarComponent} from 'ng-fullcalendar';
+import {Options} from 'fullcalendar';
+import {Router, RouterStateSnapshot} from '@angular/router';
 
 @Component({
   selector: 'app-classes',
@@ -25,7 +26,7 @@ export class ClassesComponent implements OnInit {
   calendarOptions: Options;
   @ViewChild(CalendarComponent) ucCalendar: CalendarComponent;
 
-  constructor(private _dataService: DataService) {
+  constructor(private _dataService: DataService, private router: Router ) {
   }
 
   ngOnInit() {
@@ -88,9 +89,13 @@ export class ClassesComponent implements OnInit {
 
   private initCalendar() {
     this.calendarOptions = {
-      editable: true,
+      editable: false,
       eventLimit: false,
       fixedWeekCount: false,
+      allDaySlot: false,
+      eventColor: '#3273dc',
+      timeFormat: 'HH:mm',
+      defaultView: 'agendaWeek',
       header: {
         left: 'prev,next today',
         center: 'title',
@@ -99,9 +104,15 @@ export class ClassesComponent implements OnInit {
       events: this.classes.map(c => {
         return {
           'title': c.type.name,
-          'start': `${c.date}T${c.startTime}`
+          'start': `${c.date}T${c.startTime}:00`,
+          'end': `${c.date}T${c.endTime}:00`,
+          'path': `/admin/classes/${c._id}`
         };
       })
     };
+  }
+
+  eventClick(e) {
+    this.router.navigate([e.event.path]);
   }
 }
