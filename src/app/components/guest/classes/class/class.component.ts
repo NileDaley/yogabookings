2f41451fd7bae7f0b645028bd88971fbb4718226
Nile Daley <NileDaley@outlook.com>
2018-04-14 21:09:30 +0100

    Administrators can add and remove class attendees

--- src/app/components/guest/classes/class/class.component.ts
+++ src/app/components/guest/classes/class/class.component.ts
@@ -5,6 +5,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Class, getInstance } from '../../../../models/class';
 import { Admin } from '../../../../models/admin';
 import { Tutor } from '../../../../models/tutor';
+import { Customer } from 'app/models/customer';
+import { User, Role } from 'app/models/user';
 
 @Component({
   selector: 'app-class',
@@ -17,7 +19,11 @@ export class ClassComponent implements OnInit {
   isLoggedIn: boolean;
   identity = null;
   modalActive = false;
+  modalView = null;
   confirmTutorName: String;
+  customers: Array<Customer>;
+  filteredCustomers: Array<Customer>;
+  messages = [];
 
   constructor(
     private _dataService: DataService,
@@ -27,23 +33,57 @@ export class ClassComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.getClass();
     this.isLoggedIn = this._authService.isLoggedIn();
     if (this.isLoggedIn === true) {
       this.identity = this._authService.getToken();
     }
+
+    this.getClass()
+      .then(() => {
+        if (this.isLoggedIn && this.identity.role === 20) {
+          this.getCustomers().then(() => (this.loading = false));
+        } else {
+          this.loading = false;
+        }
+      })
+      .catch(error => console.error(error));
   }
 
-  private getClass(): void {
+  private getClass(): Promise<void> {
     const classID = this.route.snapshot.paramMap.get('id');
-    this._dataService.getClass(classID).subscribe(
-      response => {
+    return this._dataService
+      .getClass(classID)
+      .toPromise()
+      .then(response => {
         const data = response['data'];
         this._class = getInstance(data);
         this.loading = false;
-      },
-      error => console.log(error)
-    );
+      })
+      .catch(error => console.error(error));
+  }
+
+  private getCustomers(): Promise<void> {
+    return this._dataService
+      .getCustomers()
+      .toPromise()
+      .then(res => {
+        const data = res['data'];
+        this.filteredCustomers = this.customers = data
+          .filter(c => {
+            return !this._class.attendees.map(a => a._id).includes(c._id);
+          })
+          .map(c => {
+            return new Customer(
+              c._id,
+              c.forename,
+              c.surname,
+              c.phone,
+              c.gender,
+              new User(c.user._id, c.user.email, null, Role.Customer)
+            );
+          });
+      })
+      .catch(error => console.error(error));
   }
 
   getTutorLink(format: 'email' | 'phone'): string {
@@ -76,7 +116,10 @@ export class ClassComponent implements OnInit {
     );
   }
 
-  toggleModal(): void {
+  toggleModal(view?: String): void {
+    if (view) {
+      this.modalView = view;
+    }
     this.modalActive = !this.modalActive;
     this.confirmTutorName = '';
   }
@@ -99,5 +142,62 @@ export class ClassComponent implements OnInit {
         })
         .catch(error => console.log(error));
     }
+  }
+
+  filterCustomers(value: string) {
+    console.log(value);
+    if (value !== '') {
+      value = value.toLowerCase().trim();
+      this.filteredCustomers = this.customers.filter(
+        c =>
+          c.forename.toLowerCase().includes(value) ||
+          c.surname.toLowerCase().includes(value)
+      );
+    } else {
+      this.filteredCustomers = this.customers;
+    }
+  }
+
+  bookClassForCustomer(_id: string) {
+    const payload = {
+      classes: [this._class],
+      customer: this.customers.find(c => c._id === _id)
+    };
+    this._dataService.insertBookings(payload).subscribe(
+      res => {
+        this.toggleModal();
+        this.loading = true;
+        this.getClass().then(() =>
+          this.messages.push({
+            type: 'success',
+            message: 'Customer added to class'
+          })
+        );
+      },
+      error => console.error(error)
+    );
+  }
+
+  removeCustomer(customer: Customer) {
+    this.toggleModal();
+    this.loading = true;
+    this._dataService
+      .cancelBookings({
+        classes: [this._class],
+        customer
+      })
+      .toPromise()
+      .then(() => {
+        this.getClass()
+          .then(() => {
+            this.getCustomers();
+            this.messages.push({
+              type: 'success',
+              message: 'Customer removed from class'
+            });
+          })
+          .catch(error => console.error(error));
+      })
+      .catch(error => console.error(error));
   }
 }
